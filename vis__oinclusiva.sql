65f2ae0d04d7465c166f4315748d20520dfc85bd
RuBeNsMoTa13 <rubens13mota@gmail.com>
2023-11-13 17:38:57 -0300

    fix:1.3.1
    mudanças em BD

---
+++ vis__oinclusiva.sql
@@ -0,0 +1,136 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 19/10/2023 às 02:34
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `visãoinclusiva`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cadastro_pessoal`
+--
+
+CREATE TABLE `cadastro_pessoal` (
+  `id` int(11) NOT NULL,
+  `nome_completo` varchar(255) NOT NULL,
+  `CPF` char(11) NOT NULL,
+  `date_nasc` date DEFAULT NULL,
+  `telefone` varchar(15) DEFAULT NULL,
+  `recado` varchar(15) DEFAULT NULL,
+  `CID` varchar(10) DEFAULT NULL,
+  `rua` varchar(255) DEFAULT NULL,
+  `numero` varchar(10) DEFAULT NULL,
+  `cidade` varchar(100) DEFAULT NULL,
+  `estado` varchar(50) DEFAULT NULL,
+  `CEP` char(8) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  `curriculo` longblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cadastro_pj`
+--
+
+CREATE TABLE `cadastro_pj` (
+  `id` int(11) NOT NULL,
+  `nome_empresa` varchar(255) NOT NULL,
+  `CNPJ` char(14) NOT NULL,
+  `data_fundacao` date DEFAULT NULL,
+  `telefone` varchar(15) DEFAULT NULL,
+  `ramo_empresa` varchar(100) DEFAULT NULL,
+  `rua` varchar(255) DEFAULT NULL,
+  `numero` varchar(10) DEFAULT NULL,
+  `cidade` varchar(100) DEFAULT NULL,
+  `estado` varchar(50) DEFAULT NULL,
+  `CEP` char(8) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cadastro_vagas`
+--
+
+CREATE TABLE `cadastro_vagas` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `empresa` varchar(255) DEFAULT NULL,
+  `localizacao` varchar(100) DEFAULT NULL,
+  `salario` decimal(10,2) DEFAULT NULL,
+  `tipo_contrato` varchar(50) DEFAULT NULL,
+  `requisitos` text DEFAULT NULL,
+  `imagem` varchar(100) NOT NULL
+
+); ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cadastro_pessoal`
+--
+ALTER TABLE `cadastro_pessoal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `cadastro_pj`
+--
+ALTER TABLE `cadastro_pj`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `cadastro_vagas`
+--
+ALTER TABLE `cadastro_vagas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cadastro_pessoal`
+--
+ALTER TABLE `cadastro_pessoal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `cadastro_pj`
+--
+ALTER TABLE `cadastro_pj`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `cadastro_vagas`
+--
+ALTER TABLE `cadastro_vagas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
