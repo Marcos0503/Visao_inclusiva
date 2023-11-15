8eddf189f3b6a9fbfacfef0a2ee51f9228770d1e
samira1406 <samiraresendef@gmail.com>
2023-11-15 19:16:13 -0300

    Tela editar pessoa juridica 1.3.4
    Adicionando documento php com a função de update de dados

---
+++ editarPerfilEmpresa/editarEmpresa.php
@@ -0,0 +1,22 @@
+<?php 
+function atualizarDados($nome, $telefone, $endereco, $email) {
+  // Conecte-se ao banco de dados
+  $conn = new PDO("mysql:host=localhost;dbname=visãoinclusiva", "root", "");
+
+  // Prepare a instrução SQL
+  $stmt = $conn->prepare("UPDATE clientes SET nome = ?, telefone = ? WHERE id = ?");
+
+  // Vincule os parâmetros
+  $stmt->bindParam(1, $nome);
+  $stmt->bindParam(2, $telefone);
+  $stmt->bindParam(3, $endereco);
+  $stmt->bindParam(4, $email);
+
+  // Execute a instrução SQL
+  $stmt->execute();
+
+  // Feche a conexão com o banco de dados
+  $conn = null;
+}
+
+?>
