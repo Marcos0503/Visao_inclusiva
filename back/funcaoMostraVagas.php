1f1c43870ffde5142450592d07b4c7863b55177c
RuBeNsMoTa13 <rubens13mota@gmail.com>
2023-11-16 21:35:02 -0300

    fix: alteração dos arquivos

--- back/funcaoMostraVagas.php
+++ back/funcaoMostraVagas.php
@@ -1,8 +1,9 @@
 <?php
+
 $host = "localhost";
 $user = "root";
 $password = ""; // Senha do seu banco de dados, se houver
-$database = "visao_inclusiva"; // Nome do seu banco de dados
+$database = "visaoinclusiva"; // Nome do seu banco de dados
 
 // Cria a conexão
 $con = mysqli_connect($host, $user, $password, $database);
