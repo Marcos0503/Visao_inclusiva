315d4aeb3cca6c9600a2101fa82ac575bd97849a
RuBeNsMoTa13 <rubens13mota@gmail.com>
2023-11-20 08:28:03 -0300

    Revert "fix:1.6.4"
    This reverts commit 01bd1dd6a86bfc9f3263682b0b8a1e3928f015f9.

--- telaVagasAdicao/alterarVagas.php
+++ telaVagasAdicao/alterarVagas.php
@@ -1,76 +1,20 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = ""; // Senha do seu banco de dados, se houver
-$database = "visaoinclusiva"; // Nome do seu banco de dados
+// ... Código para se conectar ao banco de dados ...
 
-$con = mysqli_connect($host, $user, $password, $database);
+if (isset($_POST['submit'])) {
+    $vaga_id = $_POST['vaga_id'];
+    $novo_titulo = $_POST['titulo'];
+    // Outros campos que você deseja atualizar
 
-if (!$con) {
-    die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
-}
-
-// ... (Outras partes do seu código)
-
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update'])) {
-    $id = $_POST['id']; // ID da vaga a ser atualizada
-    $titulo = $_POST['titulo'];
-    $empresa = $_POST['empresa'];
-    $localizacao = $_POST['localizacao'];
-    $salario = $_POST['salario'];
-    $periodo = $_POST['periodo'];
-    $tipo_contrato = $_POST['tipo_contrato'];
-    $descricao = $_POST['descricao'];
-    $atividades = $_POST['atividades'];
-    $beneficios = $_POST['beneficios'];
-
-    $update_query = "UPDATE cadastro_vagas SET 
-                        titulo = '$titulo', 
-                        empresa = '$empresa', 
-                        localizacao = '$localizacao', 
-                        salario = '$salario', 
-                        periodo = '$periodo', 
-                        tipo_contrato = '$tipo_contrato', 
-                        descricao = '$descricao', 
-                        atividades = '$atividades', 
-                        beneficios = '$beneficios' 
-                    WHERE id = $id";
-
-    if (mysqli_query($con, $update_query)) {
-        echo "<script>alert('Vaga atualizada com sucesso.');</script>";
-        // Redirecionar para a página de detalhes da vaga
-        header("Location: detalhesVaga.php?id=$id");
-        exit();
+    // Query SQL para atualizar os dados da vaga
+    $sql = "UPDATE cadastro_vagas SET titulo='$novo_titulo' WHERE id='$vaga_id'";
+    
+    // Executar a query de atualização
+    if (mysqli_query($con, $sql)) {
+        echo "Dados da vaga atualizados com sucesso.";
+        // Redirecionar para uma página ou fazer algo após a atualização
     } else {
-        echo "<script>alert('Erro ao atualizar a vaga. Por favor, tente novamente mais tarde.');</script>";
-        echo "Erro de atualização: " . mysqli_error($con);
+        echo "Erro ao atualizar os dados da vaga: " . mysqli_error($con);
     }
 }
-
-// Recuperar os detalhes da vaga para edição
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-
-    $query = "SELECT * FROM cadastro_vagas WHERE id = $id";
-    $result = mysqli_query($con, $query);
-
-    if ($result && mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
-        $titulo = $row['titulo'];
-        $empresa = $row['empresa'];
-        $localizacao = $row['localizacao'];
-        $salario = $row['salario'];
-        $periodo = $row['periodo'];
-        $tipo_contrato = $row['tipo_contrato'];
-        $descricao = $row['descricao'];
-        $atividades = $row['atividades'];
-        $beneficios = $row['beneficios'];
-    } else {
-        echo "Nenhum resultado encontrado.";
-    }
-}
-
-
-
-mysqli_close($con); // Fechar a conexão com o banco de dados
 ?>
