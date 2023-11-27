c8a437586396d6e1d92a76c90e5ed73826b193b5
RuBeNsMoTa13 <rubens13mota@gmail.com>
2023-11-27 20:24:03 -0300

    fix

--- telaVagas/telaVagas.php
+++ telaVagas/telaVagas.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+include "conexao.php";
+$id_usuario = isset($_SESSION['id_usuario']) ? $_SESSION['id_usuario'] : null;
+
+include("funcaoMostraVagas.php");
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -9,14 +19,19 @@
     <meta name="description" content="Seja bem vindo ao Visão Inclusiva, um site onde você pode encontrar vagas de emprego para pessoas com
         deficiência e neurodivergentes">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="form-validation.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-r4W5EeVvqpvj6Z1Fz5ATqa5AqOKVbl6bNQQl3h3D6Zs5STevB7uOs6bB8rXOBn1u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha384-r4W5EeVvqpvj6Z1Fz5ATqa5AqOKVbl6bNQQl3h3D6Zs5STevB7uOs6bB8rXOBn1u" crossorigin="anonymous">
 
     <style>
+        /* pra baixo nav bar */
         .bg {
             background-color: #2C5DAE;
             /* Cor de fundo do collapse */
@@ -30,16 +45,71 @@
             background-color: #2C5DAE;
             /* Cor de fundo do cabeçalho */
             min-height: 80px;
-            position: relative;
-            /* Adicione a propriedade position */
-            z-index: 1000;
         }
 
         .logo {
             margin-right: 5px;
-            width: 120px;
-            height: 50px;
         }
+
+        .profile-icon {
+            width: 40px;
+            height: 40px;
+            overflow: hidden;
+            border-radius: 50%;
+            border: 1px solid #000;
+
+        }
+
+        .profile-icon img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .navbar-nav .dropdown-toggle::after {
+            display: none;
+        }
+
+        .navbar-nav .dropdown-toggle::before {
+            display: none;
+        }
+
+        .navbar-nav .nav-link {
+            display: flex;
+            align-items: center;
+        }
+
+
+        .navbar-toggler {
+
+            margin-left: 40%;
+        }
+
+
+        .dropdown-item {
+            height: 100%;
+        }
+
+        .dropdown-menu {
+            text-align: center;
+            border-radius: 10px;
+            display: none;
+            background-color: #2C5DAE;
+            border: none;
+        }
+
+        .dropdown-menu button {
+            width: 100%;
+            text-align: left;
+            display: block;
+            padding: 8px 16px;
+            border-radius: 10px;
+            margin-bottom: 8px;
+            background-color: #C2D7E8;
+            color: #fff;
+        }
+
+        /* pra cima nav bar */
 
         /*Aplicando em todo documento*/
 
@@ -62,8 +132,58 @@
         body,
         html {
             height: 100%;
-            border-bottom: 10%;
         }
+
+
+        /*Ajeitando o rodapé*/
+        #content-wrap {
+            max-height: 100vh;
+            /* Defina a altura máxima como a altura da viewport (100% da altura visível) */
+            overflow-y: auto;
+            /* Adiciona uma barra de rolagem vertical quando o conteúdo excede a altura máxima */
+            padding-bottom: 2.5rem;
+            /* Adicione espaço para o rodapé */
+        }
+
+        /* Estilização da barra de rolagem para navegadores WebKit (Chrome, Safari) */
+        #content-wrap::-webkit-scrollbar {
+            width: 12px;
+            /* Largura da barra de rolagem */
+        }
+
+        #content-wrap::-webkit-scrollbar-thumb {
+            background-color: #2C5DAE;
+            /* Cor do botão de rolagem */
+            border-radius: 6px;
+            /* Borda do botão de rolagem */
+        }
+
+        #content-wrap::-webkit-scrollbar-track {
+            background-color: #f1f1f1;
+            /* Cor da trilha da barra de rolagem */
+        }
+
+        /* Estilização da barra de rolagem para o Firefox */
+        #content-wrap::-moz-scrollbar {
+            width: 12px;
+        }
+
+        #content-wrap::-moz-scrollbar-thumb {
+            background-color: #2C5DAE;
+            border-radius: 6px;
+        }
+
+        #content-wrap::-moz-scrollbar-track {
+            background-color: #f1f1f1;
+        }
+
+
+        #page-container {
+            position: relative;
+            min-height: 100vh;
+        }
+
+
 
         /*Estiliza as DIVs que aparece as vagas*/
 
@@ -97,8 +217,6 @@
             margin-right: 20px;
             /* Adiciona margem à direita para afastar da borda da página */
             margin-top: 2.5%;
-            height: 10%;
-            padding-left: 10px;
         }
 
         #search-input {
@@ -106,44 +224,25 @@
             height: 5%;
             margin-right: 1%;
             border-radius: 15px;
-            border: none;
-            box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
-            /* Adiciona uma sombra sutil */
-            z-index: 999;
-            /* Adicione um z-index menor que o do header */
         }
 
-        button {
-            height: 10%;
-            border: none;
-            box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
-            /* Adiciona uma sombra sutil */
-            z-index: 999;
-            /* Adicione um z-index menor que o do header */
-        }
 
         #search-bar button img {
-            width: 18px;
-            height: 18px;
-            z-index: 999;
-            /* Adicione um z-index menor que o do header */
+            width: 20px;
+            height: 20px;
         }
 
         #pesquisaSugestao {
             width: 30%;
             float: right;
             position: absolute;
-            top: 15%;
+            top: 0;
             right: 0;
             border-radius: 15px;
-            z-index: 999;
-            /* Adicione um z-index menor que o do header */
         }
 
         #pesquisaSugestao h3 {
             padding-left: 10px;
-            z-index: 999;
-            /* Adicione um z-index menor que o do header */
         }
 
         #botaoVagas {
@@ -166,69 +265,28 @@
             width: 90%;
         }
 
-        .profile-icon {
-            width: 40px;
-            /* Ajuste conforme necessário */
-            height: 40px;
-            /* Ajuste conforme necessário */
-            overflow: hidden;
-            border-radius: 50%;
-            border: 1px solid #000;
+        #logoRodape {
+            width: 120px;
+            height: 50px;
         }
 
-        .profile-icon img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            /* Garante que a imagem cubra completamente o círculo */
-        }
-
-        /* Remova a seta padrão e personalize a seta do dropdown */
-        .navbar-nav .dropdown-toggle::after {
-            display: none;
-
-        }
-
-        .navbar-nav .dropdown-toggle::before {
-            display: none;
-        }
-
-        a,
-        img {
-            display: block;
-        }
-
-        .navbar-nav .nav-link {
-            display: flex;
-            align-items: center;
-        }
-
-        .dropdown-item {
-            height: 100%;
-        }
-
-        .dropdown {
-            position: relative;
-            display: inline-block;
-        }
-
-        .dropdown-menu {
-            margin-left: auto;
-            border-radius: 10px;
-            background-color: transparent;
-            border: none;
-            width: 10%;
-        }
-
-        .dropdown-menu button {
-            text-align: left;
-            /* Para alinhar o texto do botão à esquerda */
-            display: block;
-            padding: 8px 16px;
-            border-radius: 10px;
-            margin-bottom: 8px;
-            background-color: #C2D7E8;
+        .bnt-link {
+            justify-content: space-between;
+            background-color: #1F78D1;
             color: #fff;
+            border-radius: 10px;
+            text-decoration: none;
+            /* Remove o sublinhado padrão */
+            /* Define como um elemento em linha com bloco */
+            padding: 10px 20px;
+            /* Adapte o preenchimento conforme necessário */
+            text-align: center;
+            /* Centraliza o texto */
+            font-weight: bold;
+            /* Define o peso da fonte como negrito */
+            border: none;
+            margin: 0.5%;
+            height: 20%;
             cursor: pointer;
         }
 
@@ -262,30 +320,80 @@
             /* Mudar cor de fundo ao passar o mouse sobre o botão */
         }
     </style>
-
     <title>Vagas | Visão Inclusiva</title>
 </head>
 
 <body responsive>
-    <header>
+<header>
+    <div class="collapse bg" id="navbarHeader">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-md-7 py-4">
+            <h4 class="text-white">Sobre</h4>
+            <p class="text">A Visão Inclusiva é um site de empregabilidade exclusivo para Pessoas com
+              Deficiência (PCD). Conectamos candidatos PCD a oportunidades de emprego, permitindo que
+              empresas anunciem vagas direcionadas a esse público. Os candidatos criam perfis detalhados,
+              enquanto as empresas podem cadastrar suas vagas especificamente para PCD, promovendo a
+              inclusão e diversidade no ambiente de trabalho, facilitando o recrutamento e seleção de
+              forma mais inclusiva..</p>
+          </div>
+          <div class="col-sm-4 offset-md-1 py-4">
+            <h4 class="text-white">Contato</h4>
+            <ul class="list-unstyled">
+              <li><a href="#" class="text-white">Me siga no Instagram</a></li>
+              <li><a href="#" class="text-white">Curta no Facebook</a></li>
+              <li><a href="#" class="text-white">InclusivaVisao@gmail.com</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
         <div class="navbar navbar-dark shadow-sm">
             <div class="container d-flex justify-content-between">
-                <a href="#" class="navbar-brand d-flex align-items-center">
-                    <img class="logo" src="../img/logoFinal.png" alt="Logo Visão Inclusiva" height="35" width="85">
+                <a href="#" class="navbar-brand d-flex align-items-center"
+                    onclick="window.location.href='../telaInicial/telaInicial.php'">
+                    <img class="logo" src="../img/logo1.png" alt="Sua Logo" height="40" width="40">
+                    <strong>Visão Inclusiva</strong>
                 </a>
-                <div class="dropdown">
-                    <a class="nav-link" href="#" role="button" onclick="toggleDropdown()">
-                        <div class="profile-icon">
-                            <img src="../img/userBase.png" alt="Perfil">
-                        </div>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="perfilDropdown">
-                        <button class="dropdown-item" onclick="irParaCurriculo()">Ir para Currículo</button>
-                        <button class="dropdown-item" onclick="irParaPerfil()">Ir para Perfil</button>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader"
+                    aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <a href="#" class="text-white">Sobre nós</a>
+                </button>
+                <div class="navbar-nav ml-auto">
+
+                    <div class="dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" id="perfilDropdown"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                            onclick="window.location.href='../telaPerfilF/visualizacaoFisica.php'">
+                            <div class="profile-icon">
+
+                                <!-- Adicione uma imagem de perfil ou ícone de usuário padrão -->
+                                <?php
+
+
+                                include "conexao.php";
+                                $id_usuario = isset($_SESSION['id_usuario']) ? $_SESSION['id_usuario'] : null;
+                                if ($id_usuario) {
+                                    $sql = "SELECT * FROM cadastro_pessoal WHERE id_usuario = $id_usuario;";
+
+                                    if ($conexao) {
+                                        $result = mysqli_query($conexao, $sql);
+
+                                        if ($result && mysqli_num_rows($result) == 1) {
+                                            $row = mysqli_fetch_assoc($result);
+                                            echo "<p class='company-info'>";
+                                            echo "<div class='foto-perfil'>";
+                                            echo '<img src="' . $row['caminho_foto_perfil'] . '" alt="Foto de Perfil" width="50" height="50">';
+                                            echo "</p>";
+
+                                        }
+                                    }
+                                }
+                                ?>
+                            </div>
+                        </a>
                     </div>
                 </div>
-            </div>
-        </div>
     </header>
 
     <div class="dropdown">
@@ -295,14 +403,13 @@
         </div>
     </div>
     </div>
-
+    </header>
     <div id="page-container">
 
         <div id="content-wrap">
             <div id="divVagas">
                 <!-- o resto do conteúdo da página -->
                 <!-- <h3>Vagas Disponíveis</h3> -->
-                <?php include("funcaoMostraVagas.php"); ?>
             </div>
         </div>
 
@@ -419,7 +526,7 @@
             var limiteSugestoes = 3;
 
             // Filtrar vagas que ainda não foram exibidas
-            var vagasNaoExibidas = vagas.filter(function(vaga) {
+            var vagasNaoExibidas = vagas.filter(function (vaga) {
                 return !exibidas.includes(vaga.id);
             });
 
@@ -463,7 +570,7 @@
         exibirSugestoes([]);
 
         // Adicione a seguinte função para ouvir eventos de teclas no campo de pesquisa
-        document.getElementById("search-input").addEventListener("keyup", function() {
+        document.getElementById("search-input").addEventListener("keyup", function () {
             searchVagas();
         });
 
@@ -549,11 +656,6 @@
             window.location.href = '../telaPerfilF/visualizacaoFisica.php';
         }
     </script>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-Uo7DzGnMz9ATKxIep9tiCxS/Z9fNfEXZJT3MXP7iNH49yIexK3MciF8HZJ3Z5mwC" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-pt>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
